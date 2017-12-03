f288ca9ce53cfeace08d24f7a38e0a9659eb3945
Tomasz Adamski <tadamski@redhat.com>
2017-12-03 16:10:17 +0100

    Long id

--- src/main/java/org/packt/swarm/petstore/api/order/Order.java
+++ src/main/java/org/packt/swarm/petstore/api/order/Order.java
@@ -7,18 +7,18 @@ import java.util.List;
 
 public class Order {
 
-    private Integer id;
+    private Long id;
 
     private int customerId;
     private final List<Item> items = new ArrayList<>();
     @JsonIgnore
     private double price;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
